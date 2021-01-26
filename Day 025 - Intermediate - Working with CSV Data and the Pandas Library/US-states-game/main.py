2c9add0dca0a487407cbaa82731d0d190700a2c3
Md.Ghulam Azad Ansari <md.ghulamazadansari786@gmail.com>
2021-01-26 06:10:53 +0530

    Apply List Comprehension

--- Day 025 - Intermediate - Working with CSV Data and the Pandas Library/US-states-game/main.py
+++ Day 025 - Intermediate - Working with CSV Data and the Pandas Library/US-states-game/main.py
@@ -15,12 +15,8 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 Guess the State",
                                     prompt="What's another state's name?").title()
     if answer_state == "Exit":
-        missing_states = []
-
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
-
+        missing_states = [
+            state for state in all_states if state not in guessed_states]
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
@@ -32,4 +28,3 @@ while len(guessed_states) < 50:
         state_date = data[data.state == answer_state]
         t.goto(int(state_date.x), int(state_date.y))
         t.write(answer_state)
-
