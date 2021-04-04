23881534b6c17c2d628a6e768664a130a95f3d03
Md.Ghulam Azad Ansari <md.ghulamazadansari786@gmail.com>
2021-04-04 13:46:14 +0530

    day 91

--- Codes/Day-091/PDF_to_Audiobook/app/pdfToAudio.py
+++ Codes/Day-091/PDF_to_Audiobook/app/pdfToAudio.py
@@ -1,18 +1,19 @@
 from gtts import gTTS
 import PyPDF2
 import pdfplumber
+import os
 
 
 class PDFToAudio:
     ENGLISH = 'en'
     HINDI = 'hi'
 
-    def pdf_to_audio(self, pdf_file, lang):
+    def pdf_to_audio(self, file, lang, filename='audio'):
         """
-            Convert PDF to audio 
+            Convert PDF to audio
             Return: Name and location of audio file
         """
-        file = open(pdf_file, 'rb')
+        audio_filename = f"{filename}.mp3"
         pdfReader = PyPDF2.PdfFileReader(file)
         pages = pdfReader.numPages
         text = ''
@@ -23,5 +24,6 @@ class PDFToAudio:
 
         if text:
             tts = gTTS(text, lang=lang)
-            tts.save('audio.mp3')
-        return 'audio.mp3'
+            tts.save(os.path.join('static', audio_filename))
+
+        return audio_filename
